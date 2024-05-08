1db4e4c48ca2efabdd23a92b93bfce6ad0e276b0
DHANESWAR BHAKAT <dhaneswarbhakat2000@gmail.com>
2024-05-08 10:15:56 +0530

    refactor: Move calculator.dart to calculator folder

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,4 @@
-import 'package:calculator_app/views/calculator.dart';
+import 'package:calculator_app/views/calculator/calculator.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
